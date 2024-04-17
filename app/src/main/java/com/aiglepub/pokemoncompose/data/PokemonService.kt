b5448a3edfa733f0a1900e97e5645138cffe4bd2
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-17 18:37:06 +0200

    Agregado nuevo servicio de API

--- app/src/main/java/com/aiglepub/pokemoncompose/data/PokemonService.kt
+++ app/src/main/java/com/aiglepub/pokemoncompose/data/PokemonService.kt
@@ -1,9 +1,13 @@
 package com.aiglepub.pokemoncompose.data
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokemonService {
 
     @GET("pokemon?limit=1302")
     suspend fun fetchAllPokemons(): RemoteResult
+
+    @GET("pokemon/{pokemonName}")
+    suspend fun fetchPokemonByName(@Path("pokemonName") name: String): RemoteFullPokemon
 }
