15958f91870aaf2df374830478accc32ada945b9
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-17 15:57:59 +0200

    Agregados permisos de Internet y mi primer composable de Navegacion

--- app/src/main/java/com/aiglepub/pokemoncompose/ui/navigation/Navigation.kt
+++ app/src/main/java/com/aiglepub/pokemoncompose/ui/navigation/Navigation.kt
@@ -1,8 +1,19 @@
 package com.aiglepub.pokemoncompose.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.aiglepub.pokemoncompose.ui.screens.PokemonScreen
 
 @Composable
 fun Navigation() {
 
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "pokemonHome" ) {
+        composable("pokemonHome") {
+            PokemonScreen()
+        }
+    }
 }
