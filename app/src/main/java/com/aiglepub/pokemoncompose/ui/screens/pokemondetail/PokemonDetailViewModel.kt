9bfcebfd69aaf43f6e203eb6d36336aeaa94f42f
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-17 19:22:22 +0200

    Creado DetailViewModel

---
+++ app/src/main/java/com/aiglepub/pokemoncompose/ui/screens/pokemondetail/PokemonDetailViewModel.kt
@@ -0,0 +1,30 @@
+package com.aiglepub.pokemoncompose.ui.screens.pokemondetail
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aiglepub.pokemoncompose.data.Pokemon
+import com.aiglepub.pokemoncompose.data.PokemonRepository
+import kotlinx.coroutines.launch
+
+class PokemonDetailViewModel(private val name: String): ViewModel() {
+
+    private val repository = PokemonRepository()
+
+    var state by mutableStateOf(UiState())
+        private set
+
+    init {
+        viewModelScope.launch {
+            state = UiState(loading = true)
+            state = UiState(loading = false, pokemon = repository.fetchPokemonByName(name))
+        }
+    }
+
+    data class UiState(
+        val loading: Boolean = false,
+        val pokemon: Pokemon? = null
+    )
+}
