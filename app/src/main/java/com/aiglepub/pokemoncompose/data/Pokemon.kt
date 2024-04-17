974cd88183369c0588cf068a9eebf26b4306c1ac
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-04-17 15:46:07 +0200

    Creado modelo de datos de la app

---
+++ app/src/main/java/com/aiglepub/pokemoncompose/data/Pokemon.kt
@@ -0,0 +1,15 @@
+package com.aiglepub.pokemoncompose.data
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val poster: String
+)
+
+val pokemonsMock = (1..100).map {
+    Pokemon(
+        id = it,
+        name = "Movie $it",
+        poster = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$it.png"
+    )
+}
