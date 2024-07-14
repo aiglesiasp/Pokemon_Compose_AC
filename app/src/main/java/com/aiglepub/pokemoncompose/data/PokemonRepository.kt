f4f2f1128bd33f1f8babf1ed488e8a4f9d9111df
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-07-14 23:14:30 +0200

    Modificado Repository tras incorporación de Flows

--- app/src/main/java/com/aiglepub/pokemoncompose/data/PokemonRepository.kt
+++ app/src/main/java/com/aiglepub/pokemoncompose/data/PokemonRepository.kt
@@ -2,12 +2,22 @@ package com.aiglepub.pokemoncompose.data
 
 import com.aiglepub.pokemoncompose.data.datasource.local.PokemonLocalDataSource
 import com.aiglepub.pokemoncompose.data.datasource.remote.PokemonRemoteDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.transform
 
 class PokemonRepository(
     private val pokemonRemoteDataSource: PokemonRemoteDataSource,
     private val pokemonLocalDataSource: PokemonLocalDataSource
 ) {
 
+    val pokemons: Flow<List<Pokemon>> = pokemonLocalDataSource.pokemons.transform { localPokemons ->
+        val pokemons = localPokemons.takeIf { it.isNotEmpty() }
+            ?: pokemonRemoteDataSource.fetchAllPokemons().also {
+                pokemonLocalDataSource.insertPokemons(it)
+            }
+        emit(pokemons)
+    }
+    /*
     suspend fun fetchAllPokemons(): List<Pokemon>
     {
         if (pokemonLocalDataSource.isEmpty()) {
@@ -16,7 +26,20 @@ class PokemonRepository(
         }
         return pokemonLocalDataSource.getAllPokemons()
     }
+     */
 
+
+
+
+    fun fetchPokemonByName(name: String): Flow<Pokemon?> = pokemonLocalDataSource.getPokemonByName(name).transform { localPokemon ->
+        val pokemon = localPokemon.takeIf { it != null }
+            ?: pokemonRemoteDataSource.fetchPokemonByName(name).also {
+            pokemonLocalDataSource.insertPokemons(listOf(it))
+        }
+        emit(pokemon)
+    }
+
+    /*
     suspend fun fetchPokemonByName(name: String): Pokemon
     {
         if (pokemonLocalDataSource.getPokemonByName(name) == null) {
@@ -25,6 +48,7 @@ class PokemonRepository(
         }
         return checkNotNull(pokemonLocalDataSource.getPokemonByName(name))
     }
+     */
 }
 
 
