1c78dc2fac98fd39017f5be71c90be6701e9e8b0
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-07-20 00:22:58 +0200

    Transformando de flow a StateFlow en el HomeViewModel

--- app/src/main/java/com/aiglepub/pokemoncompose/ui/screens/pokemonhome/PokemonViewModel.kt
+++ app/src/main/java/com/aiglepub/pokemoncompose/ui/screens/pokemonhome/PokemonViewModel.kt
@@ -5,25 +5,35 @@ import androidx.lifecycle.viewModelScope
 import com.aiglepub.pokemoncompose.data.Pokemon
 import com.aiglepub.pokemoncompose.data.PokemonRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class PokemonViewModel(
     private val repository: PokemonRepository
 ): ViewModel() {
 
-    private val _state: MutableStateFlow<UiState> = MutableStateFlow(UiState())
-    val state: StateFlow<UiState> = _state.asStateFlow()
+    val state: StateFlow<UiState> = repository.pokemons
+        .map { UiState(pokemons = it) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = UiState(loading = true)
+        )
 
     fun onUiReady() {
-        viewModelScope.launch {
+        /*viewModelScope.launch {
             _state.value = UiState(loading = true)
             repository.pokemons.collect { listPokemons ->
                 _state.value = UiState(loading = false, pokemons = listPokemons)
             }
             //_state.value = UiState(loading = false, pokemons = repository.fetchAllPokemons())
         }
+
+         */
     }
 
     data class UiState(
