300d6cf73609e3559df2bbe83fb4171ba22244c1
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-07-28 15:03:36 +0200

    Creado Modulos para Room y Retrofit

---
+++ app/src/main/java/com/aiglepub/pokemoncompose/di/AppModule.kt
@@ -0,0 +1,2 @@
+package com.aiglepub.pokemoncompose.di
+
---
+++ app/src/main/java/com/aiglepub/pokemoncompose/di/remote/network/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.aiglepub.pokemoncompose.di.remote.network
+
+import android.content.Context
+import android.location.Geocoder
+import com.aiglepub.pokemoncompose.framework.api.PokemonClient
+import com.aiglepub.pokemoncompose.framework.api.PokemonService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun providePokemonClient(): PokemonService = PokemonClient().instance
+
+    @Provides
+    fun provideFusedLocationproviderClient(@ApplicationContext context: Context): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    @Provides
+    fun provideGeocoder(@ApplicationContext context: Context): Geocoder {
+        return Geocoder(context)
+    }
+}
