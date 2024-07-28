f154e25b66eaf75c15aaad81d7cc71f2c6004952
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-07-28 14:07:33 +0200

    Agregado inject constructor en UseCases

--- app/src/main/java/com/aiglepub/pokemoncompose/domain/usecases/FetchPokemonByNameUseCase.kt
+++ app/src/main/java/com/aiglepub/pokemoncompose/domain/usecases/FetchPokemonByNameUseCase.kt
@@ -1,10 +1,11 @@
 package com.aiglepub.pokemoncompose.domain.usecases
 
+import com.aiglepub.pokemoncompose.domain.PokemonRepository
 import com.aiglepub.pokemoncompose.domain.entities.Pokemon
-import com.aiglepub.pokemoncompose.data.PokemonRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class FetchPokemonByNameUseCase (
+class FetchPokemonByNameUseCase @Inject constructor(
     private val pokemonRepository: PokemonRepository
 ) {
     operator fun invoke(name: String): Flow<Pokemon> = pokemonRepository.fetchPokemonByName(name)
--- app/src/main/java/com/aiglepub/pokemoncompose/domain/usecases/FetchPokemonsUseCase.kt
+++ app/src/main/java/com/aiglepub/pokemoncompose/domain/usecases/FetchPokemonsUseCase.kt
@@ -1,10 +1,11 @@
 package com.aiglepub.pokemoncompose.domain.usecases
 
+import com.aiglepub.pokemoncompose.domain.PokemonRepository
 import com.aiglepub.pokemoncompose.domain.entities.Pokemon
-import com.aiglepub.pokemoncompose.data.PokemonRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class FetchPokemonsUseCase(
+class FetchPokemonsUseCase @Inject constructor(
     private val pokemonRepository: PokemonRepository
 ) {
     operator fun invoke(): Flow<List<Pokemon>> = pokemonRepository.pokemons
