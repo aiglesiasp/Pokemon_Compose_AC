fe820e094ae71cc60004a3368e034de25767f26f
aiglesiasp <iglesiaspubillaitor@gmail.com>
2024-08-17 13:19:22 +0200

    Finalizado test useCases

---
+++ app/src/test/java/com/aiglepub/pokemoncompose/domain/usecases/FetchPokemonByNameUseCaseTest.kt
@@ -0,0 +1,24 @@
+package com.aiglepub.pokemoncompose.domain.usecases
+
+import kotlinx.coroutines.flow.flowOf
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+class FetchPokemonByNameUseCaseTest {
+    @Test
+    fun `Invoke should return pokemon by Name from repository`() {
+        //GIVEN
+        val pokemonFlow = flowOf(generateSamplePokemon(1))
+        val useCase = FetchPokemonByNameUseCase(mock {
+            on { fetchPokemonByName("Name 1") } doReturn pokemonFlow
+        })
+
+        //WHEN
+        val result = useCase("Name 1")
+
+        //THEN
+        assertEquals(pokemonFlow, result)
+    }
+}
